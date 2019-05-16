a5cf57df164fa73ace647ff1314bd6e4b35f58a3
Diana Navarro <navarroruiz.diana@gmail.com>
2019-05-16 17:10:29 -0500

    Add styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NgZorroAntdModule
   ]
 })
 export class SharedModule { }
