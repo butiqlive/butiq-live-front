5f753025a9e9429b1da08f2def719e31218aae1f
Diana Navarro <navarroruiz.diana@gmail.com>
2019-06-04 17:33:02 -0500

    Include contact form structure

--- src/app/account/core/components/contact/contact.component.ts
+++ src/app/account/core/components/contact/contact.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact',
+  selector: 'contact-form',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public contactFG: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  reset(){
+    this.contactFG = this.fb.group({
+      firstName: ['',[Validators.required]],
+      lastName: ['',[Validators.required]],
+      address: ['',[Validators.required]],
+      city: ['',[Validators.required]],
+      state: ['',[Validators.required]],
+      country: ['',[Validators.required]],
+      phoneNumber: ['',[Validators.required]],
+      emergencyName: ['',[Validators.required]],
+      emergencyPhoneNumber: ['',[Validators.required]],
+      gender: ['',[Validators.required]],
+      birthDate: ['',[Validators.required]],
+      weight: ['',[Validators.required]],
+    })
+  }
 
   ngOnInit() {
+    this.reset();
   }
+
+  save(){}
 
 }
--- src/app/account/core/core.module.ts
+++ src/app/account/core/core.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule as AppSharedModule } from '../../shared/shared.module';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppSharedModule
+  ],
+  exports: [
+    ContactComponent
   ]
 })
 export class CoreModule { }
