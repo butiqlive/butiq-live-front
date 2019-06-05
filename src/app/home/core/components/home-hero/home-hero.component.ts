f8783a1d75dd186b341cc4e9ffa0e010be1f629c
Diana Navarro <navarroruiz.diana@gmail.com>
2019-06-05 00:00:34 -0500

    Include home hero structure

--- src/app/home/core/components/home-hero/home-hero.component.ts
+++ src/app/home/core/components/home-hero/home-hero.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeHeroComponent implements OnInit {
 
+  public config: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.config = {
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      paginationClickable: true,
+      navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      },
+    };
   }
-
 }
